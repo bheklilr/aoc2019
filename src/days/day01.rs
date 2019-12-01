c1a0bbe238c91c1d36b1b3d19e43cd6fc695d60e
Aaron Stevens <bheklilr2@gmail.com>
2019-12-01 14:13:38 -0600

    Reworked day01 because I was bored waiting on day 2, now uses a small struct with a custom Iterator impl

--- src/days/day01.rs
+++ src/days/day01.rs
@@ -1,9 +1,41 @@
 use crate::util::int_lines;
 
+pub struct Module {
+    pub mass: i64,
+}
+
+impl Module { 
+    pub fn new(mass: i64) -> Module {
+        Module { mass: mass }
+    }
+
+    pub fn fuel(&self) -> i64 {
+        Module::fuel_for(self.mass)
+    }
+
+    pub fn fuel_for(mass: i64) -> i64 {
+        mass / 3 - 2
+    }
+}
+
+impl Iterator for Module {
+    type Item = i64;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let f = self.fuel();
+        self.mass = f;
+        if f > 0 {
+            Some(f)
+        } else {
+            None
+        }
+    }
+}
+
 pub fn day01_a() -> Result<String, String> {
     let total = int_lines("inputs/01.txt")?
         .iter()
-        .map(|value| value / 3 - 2)
+        .map(|value| Module::fuel_for(*value))
         .sum::<i64>();
     Ok(total.to_string())
 }
@@ -11,15 +43,7 @@ pub fn day01_a() -> Result<String, String> {
 pub fn day01_b() -> Result<String, String> {
     let total = int_lines("inputs/01.txt")?
         .iter()
-        .map(|value| {
-            let mut module_total = 0;
-            let mut fuel = value / 3 - 2;
-            while fuel > 0 {
-                module_total += fuel;
-                fuel = fuel / 3 - 2;
-            }
-            module_total
-        })
+        .flat_map(|value| Module::new(*value).into_iter())
         .sum::<i64>();
     Ok(total.to_string())
 }

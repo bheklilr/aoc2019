68622a23481cc75fc7d43f9120cf0c04331c112c
Aaron Stevens <bheklilr2@gmail.com>
2019-12-01 14:33:21 -0600

    Modified benchmarks so that IO times weren't included

--- src/days/day01.rs
+++ src/days/day01.rs
@@ -32,15 +32,15 @@ impl Iterator for Module {
     }
 }
 
-fn solve_a(values: Vec<i64>) -> i64 {
+fn solve_a(values: &Vec<i64>) -> i64 {
     values.iter().map(|value| Module::fuel_for(*value)).sum()
 }
 
 pub fn day01_a() -> Result<String, String> {
-    Ok(solve_a(int_lines("inputs/01.txt")?).to_string())
+    Ok(solve_a(&int_lines("inputs/01.txt")?).to_string())
 }
 
-fn solve_b(values: Vec<i64>) -> i64 {
+fn solve_b(values: &Vec<i64>) -> i64 {
     values
         .iter()
         .flat_map(|value| Module::new(*value).into_iter())
@@ -48,7 +48,7 @@ fn solve_b(values: Vec<i64>) -> i64 {
 }
 
 pub fn day01_b() -> Result<String, String> {
-    Ok(solve_b(int_lines("inputs/01.txt")?).to_string())
+    Ok(solve_b(&int_lines("inputs/01.txt")?).to_string())
 }
 
 #[cfg(test)]
@@ -59,32 +59,36 @@ mod test {
 
     #[test]
     fn test_solve_a() -> Result<(), String> {
-        assert_eq!(solve_a(vec![0, 1, 2, 3]), 0);
-        assert_eq!(solve_a(vec![12]), 2);
-        assert_eq!(solve_a(vec![14]), 2);
-        assert_eq!(solve_a(vec![1969]), 654);
-        assert_eq!(solve_a(vec![100756]), 33583);
-        assert_eq!(solve_a(int_lines("inputs/01.txt")?), 3325347);
+        assert_eq!(solve_a(&vec![0, 1, 2, 3]), 0);
+        assert_eq!(solve_a(&vec![12]), 2);
+        assert_eq!(solve_a(&vec![14]), 2);
+        assert_eq!(solve_a(&vec![1969]), 654);
+        assert_eq!(solve_a(&vec![100756]), 33583);
+        assert_eq!(solve_a(&int_lines("inputs/01.txt")?), 3325347);
         Ok(())
     }
 
     #[test]
     fn test_solve_b() -> Result<(), String> {
-        assert_eq!(solve_b(vec![0, 1, 2, 3]), 0);
-        assert_eq!(solve_b(vec![14]), 2);
-        assert_eq!(solve_b(vec![1969]), 966);
-        assert_eq!(solve_b(vec![100756]), 50346);
-        assert_eq!(solve_b(int_lines("inputs/01.txt")?), 4985145);
+        assert_eq!(solve_b(&vec![0, 1, 2, 3]), 0);
+        assert_eq!(solve_b(&vec![14]), 2);
+        assert_eq!(solve_b(&vec![1969]), 966);
+        assert_eq!(solve_b(&vec![100756]), 50346);
+        assert_eq!(solve_b(&int_lines("inputs/01.txt")?), 4985145);
         Ok(())
     }
 
     #[bench]
-    fn bench_day01_a(b: &mut Bencher) {
-        b.iter(day01_a);
+    fn bench_solve_a(b: &mut Bencher) -> Result<(), String> {
+        let values = int_lines("inputs/01.txt")?;
+        b.iter(|| solve_a(&values));
+        Ok(())
     }
 
     #[bench]
-    fn bench_day01_b(b: &mut Bencher) {
-        b.iter(day01_b);
+    fn bench_solve_b(b: &mut Bencher) -> Result<(), String> {
+        let values = int_lines("inputs/01.txt")?;
+        b.iter(|| solve_b(&values));
+        Ok(())
     }
 }

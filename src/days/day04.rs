0ff6204167a5e85942e9c9aa8c8796408981c9be
Aaron Stevens <bheklilr2@gmail.com>
2019-12-04 06:49:55 -0600

    Solved part 1

--- src/days/day04.rs
+++ src/days/day04.rs
@@ -2,11 +2,15 @@ const START: i32 = 165432;
 const STOP: i32 = 707912;
 
 fn has_adjacent_matching_digits(i: i32) -> bool {
-    false
+    i.to_string().chars().zip(i.to_string()[1..].chars())
+        .filter(|(x, y)| x == y)
+        .collect::<Vec<_>>()
+        .len() > 0
 }
 
 fn has_monotonically_increasing_digits(i: i32) -> bool {
-    false
+    i.to_string().chars().zip(i.to_string()[1..].chars())
+        .all(|(x, y)| x <= y)
 }
 
 pub fn day04_a() -> Result<String, String> {

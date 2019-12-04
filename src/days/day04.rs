3206b465bd66be2e3210551353d82f69bd11fd26
Aaron Stevens <bheklilr2@gmail.com>
2019-12-04 06:20:28 -0600

    Started work on day 4

--- src/days/day04.rs
+++ src/days/day04.rs
@@ -1,5 +1,22 @@
+const START: i32 = 165432;
+const STOP: i32 = 707912;
+
+fn has_adjacent_matching_digits(i: i32) -> bool {
+    false
+}
+
+fn has_monotonically_increasing_digits(i: i32) -> bool {
+    false
+}
+
 pub fn day04_a() -> Result<String, String> {
-    Err("Unsolved".to_string())
+    Ok((START..STOP)
+        .filter(|&i| i.to_string().len() == 6)
+        .filter(|&i| has_adjacent_matching_digits(i))
+        .filter(|&i| has_monotonically_increasing_digits(i))
+        .collect::<Vec<_>>()
+        .len()
+        .to_string())
 }
 
 pub fn day04_b() -> Result<String, String> {

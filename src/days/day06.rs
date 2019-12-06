a21ba9faf11d0aab28d6b6171754d43a3a74b096
Aaron Stevens <bheklilr2@gmail.com>
2019-12-06 06:46:22 -0600

    Started working on day 6, but butting up against the borrow checker

--- src/days/day06.rs
+++ src/days/day06.rs
@@ -1,3 +1,21 @@
+use crate::util::lines;
+use std::collections::HashMap;
+
+fn load_orbits() -> Result<HashMap<String, Vec<String>>, String> {
+    let mut map = HashMap::new();
+    for line in lines("inputs/06.txt")?.iter() {
+        let bodies: Vec<_> = line.split(',').collect();
+        let center = bodies[0].to_string();
+        let satellite = bodies[1].to_string();
+
+        let mut empty = vec![];
+        let mut existing = map.get_mut(&center).unwrap_or(&mut empty);
+        existing.push(satellite);
+        map.insert(center, existing.to_vec());
+    }
+    Ok(map)
+}
+
 pub fn day06_a() -> Result<String, String> {
     Err("Unsolved".to_string())
 }
